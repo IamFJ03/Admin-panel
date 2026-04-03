a2aad7961f73d4409f342324c4b4ee9b41e87e49
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-04-03 22:55:16 +0530

    My Records Page

--- frontend/src/pages/user/UserRecords.jsx
+++ frontend/src/pages/user/UserRecords.jsx
@@ -1,41 +1,84 @@
 import React, { useState } from 'react'
 import Sidebar from '../../components/sidebar'
-import { Search } from 'lucide-react'
+import { Search, Filter } from 'lucide-react'
 export default function UserRecords() {
-    const[category, setCategory] = useState("");
-    const[allTypes, setAllTypes] = useState("");
+    const [category, setCategory] = useState("");
+    const [allTypes, setAllTypes] = useState("");
+    const [range, setRange] = useState("");
+
+    const getMonthRanges = () => {
+        const today = new Date();
+        const ranges = [];
+
+        for (let i = 0; i < 6; i++) {
+            const start = new Date(today.getFullYear(), today.getMonth() - i, 1);
+            const end = new Date(today.getFullYear(), today.getMonth() - i + 1, 0);
+
+            ranges.push({
+                label: `${start.toLocaleString("default", { month: "long" })} ${start.getDate()} - ${end.toLocaleString("default", { month: "long" })} ${end.getDate()}`,
+                value: `${start.toISOString()}_${end.toISOString()}`,
+            });
+        }
+
+        return ranges;
+    };
+
+    const ranges = getMonthRanges();
     return (
         <div>
             <div className='flex w-screen'>
                 <Sidebar />
-                <div className='w-full m-5 rounded shadow-md'>
+                <div className='w-full m-5 rounded shadow-md flex flex-col'>
                     <div className='p-5'>
                         <p className='text-xl font-semibold'>My Records</p>
                         <p className='text-gray-500'>View and manage all your income and expense Records</p>
                     </div>
                     <div className='flex justify-between mx-5 p-3 rounded shadow-md'>
                         <div className='flex gap-3 items-center rounded bg-gray-200 p-1'>
-                            <Search size={15} color='black' />
-                              <input type='text' placeholder='Search records' className='focus:outline-none focus:border-gray-200'/>
+                            <Search size={15} color='black' className='cursor-pointer' />
+                            <input type='text' placeholder='Search records' className='focus:outline-none focus:border-gray-200' />
                         </div>
                         <div>
-                            <select value={allTypes} onChange={(e) => setAllTypes(e.target.value)} className='border border-gray-500 rounded p-1 w-[125%]'>
+                            <select value={allTypes} onChange={(e) => setAllTypes(e.target.value)} className='border border-gray-500 rounded p-1 w-[120%]'>
                                 <option value="" className='font-semibold' disabled>All Types</option>
                                 <option value="rent">Income</option>
                                 <option value="salary">Expenses</option>
                             </select>
                         </div>
                         <div>
-                            <select value={category} onChange={(e) => setCategory(e.target.value)} className='border border-gray-500 rounded p-1 w-[125%]'>
+                            <select value={category} onChange={(e) => setCategory(e.target.value)} className='border border-gray-500 rounded p-1 w-[120%]'>
                                 <option value="" className='font-semibold' disabled>All Categories</option>
                                 <option value="food">Food</option>
                                 <option value="rent">Rent</option>
                                 <option value="salary">Salary</option>
                             </select>
                         </div>
-                        
-                        <div></div>
-                        <div></div>
+
+                        <div>
+                            <select value={range} onChange={(e) => setRange(e.target.value)} className='border border-gray-500 rounded p-1'>
+                                <option value="">Select Date Range</option>
+
+                                {ranges.map((r, index) => (
+                                    <option key={index} value={r.value}>
+                                        {r.label}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className='flex items-center bg-gray-200 py-1 px-5 gap-2 rounded cursor-pointer'>
+                            <Filter size={15} color='black' />
+                            <button >Filter</button>
+                        </div>
+                    </div>
+                    <div className='m-5 shadow-lg h-full text-gray-500'>
+                        <ul className='flex justify-between p-3 bg-gray-200 rounded'>
+                            <li>Date</li>
+                            <li>Type</li>
+                            <li>Category</li>
+                            <li>Amount</li>
+                            <li>Notes</li>
+                            <li>Actions</li>
+                        </ul>
                     </div>
                 </div>
             </div>
