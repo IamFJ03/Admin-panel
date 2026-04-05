1cd95f06756b4448ae34e7b119b0d059fb2f3cfe
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-04-05 20:57:59 +0530

    Settings Page

--- frontend/src/pages/user/Settings.jsx
+++ frontend/src/pages/user/Settings.jsx
@@ -13,19 +13,21 @@ export default function Settings() {
                     <p className='text-xl font-semibold'>Profile Information</p>
                     <div className='m-5'>
                         <p className='mb-2'>Name:</p>
-                        <input type='text' placeholder='Enter Name...' className='w-70 border py-1 px-2 rounded'/>
+                        <input type='text' placeholder='Enter Name...' className='w-70 border py-1 px-2 rounded' />
                     </div>
                     <div className='m-5'>
                         <p className='mb-2'>Email:</p>
-                        <input type='text' placeholder='email@example.com' className='w-70 border py-1 px-2 rounded'/>
+                        <input type='text' placeholder='email@example.com' className='w-70 border py-1 px-2 rounded' />
                     </div>
                     <button className='bg-blue-500 text-white py-1 px-3 rounded ml-5'>Change</button>
                 </div>
                 <div className='m-5'>
                     <p className='text-xl font-semibold'>Delete Acoount</p>
                     <p>Delete your account and all of its resources</p>
-                    <div className='m-10 bg-red-300 rounded-2xl'>
-<p>hjkfhb</p>
+                    <div className='text-red-500 p-5 m-5 bg-red-200 rounded-2xl'>
+                        <p className='text-xl font-semibold'>Warning!</p>
+                        <p>Please Proceed with caution, this cannot be undone...</p>
+                        <button className='bg-red-500 text-white mt-5 px-3 py-2 rounded cursor-pointer'>Delete Account</button>
                     </div>
                 </div>
             </div>
