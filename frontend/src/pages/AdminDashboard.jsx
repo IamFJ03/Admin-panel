250227e1d5d7855eee86106b93ee707fca1e894f
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-04-06 23:21:42 +0530

    My Reports Page

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -2,57 +2,57 @@ import React, { useEffect, useState } from 'react'
 import Sidebar from '../components/sidebar';
 import { useLocation } from 'react-router-dom';
 import { ArrowUpRight, Receipt, Landmark, ArrowDownLeft } from 'lucide-react';
-import { PieChart, Pie, Tooltip,BarChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { PieChart, Pie, Tooltip, BarChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts';
 export default function AdminDashboard() {
-  const[record, setRecord] = useState({});
+  const [record, setRecord] = useState({});
   const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff7c7c", "#8dd1e1"];
   useEffect(() => {
     const token = localStorage.getItem("token");
     const loadSpecificData = async () => {
-      const res = await fetch("http://127.0.0.1:8000/api/loadAmount",{
-        method:"GET",
-        headers:{
+      const res = await fetch("http://127.0.0.1:8000/api/loadAmount", {
+        method: "GET",
+        headers: {
           Authorization: `Bearer ${token}`,
           Accept: "application/json"
         }
       });
 
       const data = await res.json();
-      if(!res.ok){
-        if(data.errors){
+      if (!res.ok) {
+        if (data.errors) {
           console.log(data.errors);
         }
-        else{
+        else {
           console.log(data.message);
         }
       }
 
-      if(data.message === "Data Fetched"){
+      if (data.message === "Data Fetched") {
         console.log(data);
         setRecord(data);
       }
     }
 
     loadSpecificData();
-  },[]);
+  }, []);
 
   const data = record?.categoryTotal?.map((item, index) => ({
     name: item.category,
     value: Number(item.total),
-    fill: colors[index%colors.length]
+    fill: colors[index % colors.length]
   }))
 
-const barData = record?.monthly?.map(item => ({
-  name: new Date(2026, item.month - 1).toLocaleString('default', { month: 'short' }),
-  income: item.income,
-  expense: item.expense
-}))
+  const barData = record?.monthly?.map(item => ({
+    name: new Date(2026, item.month - 1).toLocaleString('default', { month: 'short' }),
+    income: item.income,
+    expense: item.expense
+  }))
   const location = useLocation();
   const name = location.state?.name;
   const role = location.state?.role;
   return (
     <div className='flex'>
-      <Sidebar role={role}/>
+      <Sidebar role={role} />
       <div className='m-5 flex-3'>
         <p className='text-xl font-semibold'>Welcome, {name}!</p>
         <p className='border-b border-gray-400 pb-2 mb-2'>Here's your dashboard overview</p>
@@ -90,15 +90,14 @@ const barData = record?.monthly?.map(item => ({
           <div className='flex-1 rounded shadow-[-5px_5px_10px_rgb(0,0,0,0.5)]'>
             <p className='font-semibold m-3 text-xl'>Income Vs Expense</p>
             <ResponsiveContainer width="100%" height={300} className='mt-20'>
-<BarChart data={barData}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
-  <YAxis />
-  <Tooltip />
-  <Bar dataKey="income" fill='#36A2EB'/>
-  <Bar dataKey="expense" fill='#FF6384' />
-</BarChart>
-
+              <BarChart data={barData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="income" fill='#36A2EB' />
+                <Bar dataKey="expense" fill='#FF6384' />
+              </BarChart>
             </ResponsiveContainer>
           </div>
           <div className='flex flex-col flex-1 gap-5 rounded '>
--- frontend/src/pages/user/MyReports.jsx
+++ frontend/src/pages/user/MyReports.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
-import Sidebar from '../../components/sidebar'
+import Sidebar from '../../components/sidebar';
+import {PieChart, Pie, CartesianGrid, ResponsiveContainer, Tooltip, BarChart, Bar, XAxis, YAxis} from 'recharts';
 export default function MyRecords() {
+  const pieData = [
+    {
+      name: "Income",
+      value: 5250,
+      fill: '#007bff'
+    },
+    {
+      name: "Expense",
+      value: 2430,
+      fill: '#bff007'
+    }
+  ];
+
+  const barData = [
+    {
+      name: "April",
+      income: 5250,
+      expense: 2430
+    },
+    {}
+  ]
   const financeData = [
     {
       type: 'Income',
@@ -48,15 +70,37 @@ export default function MyRecords() {
           </select>
         </div>
         <div className='m-5'>
-          <p className='text-xl font-semibold'>Finance Overview</p>
+          <p className='text-xl font-semibold mb-5'>Finance Overview</p>
           <div className='flex justify-between gap-5'>
             {financeData.map(item => (
               <div className={`p-5 ${item.color} flex-1 rounded`}>
-<p className=''>{item.type}</p>
-<p className='text-xl font-semibold'>{item.amount}</p>
-<p className='text-sm text-gray-400'>{item.statement}</p>
+                <p className=''>{item.type}</p>
+                <p className='text-xl font-semibold'>{item.amount}</p>
+                <p className='text-sm text-gray-400'>{item.statement}</p>
               </div>
             ))}
+          </div>
+        </div>
+        <div className='flex mx-5 gap-10'>
+          <div className='flex-1'>
+            <p className='text-xl font-semibold'>Income vs Expense</p>
+            <PieChart width={350} height={250}>
+              <Pie data={pieData} outerRadius={100} dataKey="value" label>
+                <Tooltip />
+              </Pie>
+            </PieChart>
+          </div>
+          <div className='flex-1'>
+            <p className='text-xl font-semibold'>Cash Flow</p>
+            <ResponsiveContainer width="100%" height={250}>
+              <BarChart data={barData}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis />
+                <Bar dataKey="income" fill='#36A2EB'/>
+                <Bar dataKey="expense" fill='#FF6384'/>
+              </BarChart>
+            </ResponsiveContainer>
           </div>
         </div>
       </div>
