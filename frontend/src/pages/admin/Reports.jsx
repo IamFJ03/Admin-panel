7f6d6f206d141763d24d8f031bf2817e9e14df4a
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-05-16 17:10:16 +0530

    Reports Page

--- frontend/src/pages/admin/Reports.jsx
+++ frontend/src/pages/admin/Reports.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar';
-import { User, Check, UserX, UserPlus, Menu } from 'lucide-react';
+import { User, Check, UserX, UserPlus, Menu, Search } from 'lucide-react';
 import UserInfo from '../../components/UserInfo';
 export default function Reports() {
   return (
@@ -10,9 +10,30 @@ export default function Reports() {
         <span className='text-xl ml-5 font-semibold text-gray-400'>Reports</span><span className='text-xl ml-2 font-semibold'>/ Users</span>
         <p className='ml-5 text-gray-400'>View and analyze user data. Select a user to see detailed information.</p>
         <UserInfo />
-        <div className='grid grid-cols-2 gap-2 m-5'>
-          <div className='shadow-md flex-1'>ksd</div>
-          <div className='shadow-md flex-1'>jsd</div>
+        <div className=' gap-2 flex m-5 h-9/12'>
+          <div className='shadow-md p-3 flex-2'>
+            <div className='flex items-center justify-between '>
+              <div className='flex items-center border border-gray-400 rounded px-3'>
+                <input type='text' placeholder='Search by name or email...' className='w-72 p-1 outline-none rounded' />
+                <Search size={18} className='cursor-pointer' />
+              </div>
+              <div className='flex items-center gap-3 border px-3 py-1 border-gray-400 rounded'>
+                <p>Sort by:</p>
+                <select className='border-none'>
+                  <option value="">Newest</option>
+                  <option value="">Oldest</option>
+                </select>
+              </div>
+            </div>
+            <div>
+              <ul className='grid grid-cols-3 px-5 py-2 text-gray-500 bg-gray-100 my-5 rounded'>
+              <li>USER</li>
+              <li>EMAIL</li>
+              <li>ROLE</li>
+            </ul>
+            </div>
+          </div>
+          <div className='shadow-md p-3 flex-1'>jsd</div>
         </div>
       </div>
     </div>
